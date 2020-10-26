bfaae545679a167c9bd6804992c82941fcdf6d10
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2020-10-26 16:30:15 -0700

    Finished Day 1

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermoine", "Ron"]
+    @State private var selectedStudent = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Picker("Select your student", selection: $selectedStudent) {
+                ForEach(0 ..< students.count) {
+                    Text(students[$0])
+                }
+            }
+            Text("You chose \(students[selectedStudent])")
+        }
     }
 }
 
